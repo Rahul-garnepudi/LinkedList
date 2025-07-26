fe687a1f8bf673f8617a7ef1749c655a92998b87
Rahul-garnepudi <rahulgarnepudi2@gmail.com>
2025-07-26 12:32:44 +0530

    Add files via upload

---
+++ doublelinkedlist2.py
@@ -0,0 +1,32 @@
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.prev=None
+    self.next=None
+class DoublyLinkedList:
+  def __init__(self):
+    self.head=None
+  def iae(self,data):
+    newnode=Node(data)
+    if self.head is None:
+      self.head=newnode
+      return
+    temp=self.head
+    while temp.next:
+      temp=temp.next
+    temp.next=newnode
+    newnode.prev=temp
+  def display(self):
+    temp=self.head
+    print("Doubly Linked List:")
+    while temp:
+      print(temp.data,end="<->")
+      temp=temp.next
+    print("None")
+
+dll=DoublyLinkedList()
+n=int(input("Enter the number of elements:"))
+for i in range(n):
+  val=int(input(f"Enter element {i+1}:"))
+  dll.iae(val)
+dll.display()
